f7eb7c8370c5175bb534119ab0eff81ebee5a58f
damianamalraj <damian.amalraj@hotmail.com>
2022-05-18 15:55:43 +0200

    Somthing wrong with nested folder and dotgit file think i fixed it...

---
+++ react-bootcamp/src/components/tab2.jsx
@@ -0,0 +1,14 @@
+function tab2() {
+  return (
+    <section className="tab2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque excepturi
+      fugiat incidunt aut voluptas soluta aperiam beatae quis, nesciunt vero
+      inventore? Commodi quaerat facere eum repudiandae ex. Itaque unde
+      doloribus dolores. Voluptatum perspiciatis unde nobis inventore quo.
+      Dignissimos dolor, ab itaque eius voluptatem beatae corporis, veniam
+      impedit quo sunt natus.
+    </section>
+  );
+}
+
+export default tab2;
